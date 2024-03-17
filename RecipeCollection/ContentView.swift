d8da8f7c5ee55ec5d16af6bf2083ac7c2ac0d9fd
R-Szk <trail_stuff_seat@yahoo.co.jp>
2024-03-17 20:05:04 +0900

    Initial Commit

---
+++ RecipeCollection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipeCollection
+//
+//  Created by 鈴木龍斗 on 2024/03/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipeCollection/RecipeCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeCollectionApp.swift
+//  RecipeCollection
+//
+//  Created by 鈴木龍斗 on 2024/03/17.
+//
+
+import SwiftUI
+
+@main
+struct RecipeCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
