cc087d748d4686ccf656959399cc39257762d68a
R-Szk <trail_stuff_seat@yahoo.co.jp>
2024-03-25 22:33:14 +0900

    Webページを表示するViewを追加

--- RecipeCollection/View/HomeView/HomeView.swift
+++ RecipeCollection/View/HomeView/HomeView.swift
@@ -9,48 +9,51 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            // --- 最近追加したレシピ部分 ---
+//        VStack(alignment: .leading) {
+        ScrollView {
             VStack(alignment: .leading) {
-                HStack {    // タイトル
-                    Text("最近追加したレシピ")
-                        .font(.title2)
-                }   // コンテンツ
-                HStack {
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.blue)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.blue)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.blue)
+                // --- 最近追加したレシピ部分 ---
+                VStack(alignment: .leading) {
+                    HStack {    // タイトル
+                        Text("最近追加したレシピ")
+                            .font(.title2)
+                    }   // コンテンツ
+                    HStack {
+                        Rectangle()
+                            .frame(width: 100, height: 100)
+                            .foregroundStyle(.blue)
+                        Rectangle()
+                            .frame(width: 100, height: 100)
+                            .foregroundStyle(.blue)
+                        Rectangle()
+                            .frame(width: 100, height: 100)
+                            .foregroundStyle(.blue)
+                    }
                 }
+                .padding(.bottom)
+                
+                VStack(alignment: .leading) {
+                    // --- 季節に応じたレシピ ---
+                    HStack {
+                        Text("春の定番")
+                            .font(.title2)
+                        Spacer()
+                    }
+                    HStack {
+                        Rectangle()
+                            .frame(width: 100, height: 100)
+                            .foregroundStyle(.blue)
+                        Rectangle()
+                            .frame(width: 100, height: 100)
+                            .foregroundStyle(.blue)
+                        Rectangle()
+                            .frame(width: 100, height: 100)
+                            .foregroundStyle(.blue)
+                    }
+                }
+                .padding(.bottom)
+                Spacer()
             }
-            .padding(.bottom)
-            
-            VStack(alignment: .leading) {
-                // --- 季節に応じたレシピ ---
-                HStack {
-                    Text("春の定番")
-                        .font(.title2)
-                    Spacer()
-                }
-                HStack {
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.blue)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.blue)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.blue)
-                }
-            }
-            .padding(.bottom)
-            Spacer()
         }
     }
 }
---
+++ RecipeCollection/View/WebPageView.swift
@@ -0,0 +1,33 @@
+//
+//  WebPageView.swift
+//  RecipeCollection
+//
+//  Created by 鈴木龍斗 on 2024/03/25.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebPageView: UIViewRepresentable {
+    
+    let loadUrl: String
+    
+    func makeUIView(context: Context) -> some UIView {
+        guard let url = URL(string: self.loadUrl) else {
+            return WKWebView()
+        }
+        
+        let webView = WKWebView()
+        webView.load(URLRequest(url: url))
+        
+        return webView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+}
+
+#Preview {
+    WebPageView(loadUrl: "https://www.google.com")
+}
